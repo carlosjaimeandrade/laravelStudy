7775bae056bea5f64b311a5f06e0884efc569b91
Carlos jaime <jaime_andrek@hotmail.com>
2021-12-09 09:16:58 -0300

    if no blade Blade: Condicionais

--- resources/views/admin/config.blade.php
+++ resources/views/admin/config.blade.php
@@ -8,6 +8,16 @@
     meu nome é {{ $nome }} Eu tenho {{ $idade }}
     <br>
     essa é uma variavel global, versão é {{ $versao }}
+    <br>
+
+    @if($idade > 90)
+        eu sou maior de idade
+    @else
+        não sou maior de idade
+    @endif
+
+
+
     <form method="post">
         @csrf
         name:
