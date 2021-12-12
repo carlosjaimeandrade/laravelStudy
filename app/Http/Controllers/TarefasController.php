3b76d26d5094b30775bdfd6b82ca5f7b7354f6d4
Carlos jaime <jaime_andrek@hotmail.com>
2021-12-12 13:47:35 -0300

    CRUD NO LARAVEL 8 MEU JEITO (DESAFIO)

--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -12,6 +12,7 @@ class TarefasController extends Controller
     //sempre ter um para GET e para
     public function list()
     {
+        
         $list = DB::select('SELECT * FROM tarefas');
 
         return view('tarefas.list', [
@@ -43,7 +44,7 @@ class TarefasController extends Controller
             'id' => $id
         ]);
 
-        if (count($list)>0) {
+        if (count($list) > 0) {
             return view('tarefas.edit', [
                 'list' => $list
             ]);
@@ -54,8 +55,12 @@ class TarefasController extends Controller
 
     public function editAction(Request $request, $id)
     {
+       
         if ($request->filled('titulo')) {
             $titulo = $request->input('titulo');
+        }else{
+            $titulo = '';
+        }
 
             DB::update('UPDATE tarefas SET titulo=:titulo WHERE id=:id', [
                 'titulo' => $titulo,
@@ -63,15 +68,35 @@ class TarefasController extends Controller
             ]);
 
             return redirect()->route('tarefas.list');
+   
+    }
+
+    public function del($id)
+    {
+
+        DB::delete('DELETE FROM tarefas WHERE id=:id', [
+            'id' => $id
+        ]);
+        return redirect()->route('tarefas.list');
+    }
+
+    public function done($id)
+    {
+        $list = DB::select('SELECT * FROM tarefas WHERE id=:id', [
+            'id' => $id
+        ]);
+
+        if (count($list) > 0) {
+
+            $marcador =  $list[0]->resolvido == 0 ? 1 : 0;
+
+            DB::UPDATE('UPDATE tarefas SET resolvido=:marcador WHERE id=:id', [
+                'marcador' => $marcador,
+                'id' => $id
+            ]);
+            return redirect()->route('tarefas.list');
         } else {
+            return view('notfound');
         }
-    }
-
-    public function del()
-    {
-    }
-
-    public function done()
-    {
     }
 }
