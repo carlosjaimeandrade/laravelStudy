231cfba3e35d69021ef4bfcc49b10a9b1d161fb8
Carlos jaime <jaime_andrek@hotmail.com>
2022-01-05 14:30:06 -0300

    Register ajustando caso hover erros

---
+++ bd/laravel 1.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Jan-2022 às 18:59
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `laravel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tarefas`
+--
+
+CREATE TABLE `tarefas` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `resolvido` int(1) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tarefas`
+--
+
+INSERT INTO `tarefas` (`id`, `titulo`, `resolvido`) VALUES
+(26, 'testando pelo eloquent', 1),
+(25, 'testando pelo eloquent', 1),
+(10, '1', 1),
+(11, 'Dormir', 1),
+(12, '1', 1),
+(13, 'testando pelo eloquent', 1),
+(14, 'testando pelo eloquent', 1),
+(15, 'testando pelo eloquent', 1),
+(16, 'testando pelo eloquent', 1),
+(17, 'testando pelo eloquent', 1),
+(18, 'testando pelo eloquent', 1),
+(19, 'testando pelo eloquent', 1),
+(27, 'testando pelo eloquent', 1),
+(21, 'testando pelo eloquent', 1),
+(22, 'testando pelo eloquent', 1),
+(23, 'testando pelo eloquent', 1),
+(24, 'new tarefas', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `remember_token` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tarefas`
+--
+ALTER TABLE `tarefas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tarefas`
+--
+ALTER TABLE `tarefas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,19 +2,23 @@
 
 @section('title', 'Cadastro')
 
-@if(session('warning'))
-<x-alert>
-    @slot('type')
-        Error de preenchimento feito pela validação
-    @endslot
+@if ($errors->any())
+    <x-alert>
+        @slot('type')
+            Error de preenchimento feito pela validação
+        @endslot
 
-    @slot('subtitle')
-        Motivo:
-    @endslot
+        @slot('subtitle')
 
-    {{-- aqui passamos por todos os erros contido --}}
-    {{session('warning')}}
-</x-alert>
+        @endslot
+
+        {{-- aqui passamos por todos os erros contido --}}
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </x-alert>
 @endif
 
 
@@ -22,11 +26,11 @@
 
     <form method="post">
         @csrf
-        
-        <input type="text" name="nome" placeholder="digite o seu nome">
-        <input type="email" name="email" placeholder="digite seu e-mail">
+
+        <input type="text" name="nome" placeholder="digite o seu nome" value="{{ old('nome') }}">
+        <input type="email" name="email" placeholder="digite seu e-mail" value="{{ old('email') }}">
         <input type="password" name="password" placeholder="Digite sua senha">
-        <input type="password_confirmation" name="password" placeholder="Confirme sua senha">
+        <input type="password" name="password_confirmation" placeholder="Confirme sua senha">
         <input type="submit" value="entrar">
     </form>
 
